71627db2771782dd44a174114ba5007040e34e1a
Andrew Dang <o9dangson@gmail.com>
2021-04-15 22:16:50 -0400

    a

--- nutshell.c
+++ nutshell.c
@@ -1,95 +1,89 @@
-#include "stdbool.h"
-#include <limits.h>
-#include <unordered_map>
-#include <vector>
-#include <string>
+// This is ONLY a demo micro-shell whose purpose is to illustrate the need for and how to handle nested alias substitutions and how to use Flex start conditions.
+// This is to help students learn these specific capabilities, the code is by far not a complete nutshell by any means.
+// Only "alias name word", "cd word", and "bye" run.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "global.h"
+#include <unistd.h>
+#include <cstddef>
 
-struct evTable {
-   char var[128][100];
-   char word[128][100];
-};
+extern char *getcwd(char *buf, size_t size);
+int yyparse();
+int aliasIndex, varIndex;
+char cwd[PATH_MAX];
+struct evTable varTable;
+std::vector<std::string> myPaths;
+void parsePaths(const char* path);
+void clearPaths();
 
-struct aTable {
-	char name[128][100];
-	char word[128][100];
-   
-};
+int main()
+{
+    aliasIndex = 0;
+    varIndex = 0;
 
-extern char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
 
-extern struct evTable varTable;
+    strcpy(varTable.var[varIndex], "PWD");
+    strcpy(varTable.word[varIndex], cwd);
+    varIndex++;
+    strcpy(varTable.var[varIndex], "HOME");
+    strcpy(varTable.word[varIndex], cwd);
+    varIndex++;
+    strcpy(varTable.var[varIndex], "PROMPT");
+    strcpy(varTable.word[varIndex], "nutshell");
+    varIndex++;
+    strcpy(varTable.var[varIndex], "PATH");
+    const char* path = getenv("PATH");
+    strcpy(varTable.word[varIndex], path);
+    parsePaths(path);
+    if(1){
+    	std::string temp(cwd);
+	    myPaths.push_back(temp);
+    }
+    varIndex++;
 
-extern struct aTable aliasTable;
 
-extern int aliasIndex, varIndex;
 
-char* subAliases(char* name);
+    system("clear");
+    while(1)
+    {
+        printf("[%s~%s]>> ", varTable.word[2], varTable.word[0]);
+        //printf("%s\n", getcwd(cwd, sizeof(cwd)));
 
-extern std::unordered_map<std::string,std::string> aliases;
+        yyparse();
+    }
 
-extern std::unordered_map<std::string,std::string> variables;
+    //Freeing memory
+    clearPaths();
+   return 0;
+}
 
-extern std::vector<std::string> myPaths;
+void parsePaths(const char* path){
+	std::string str(path);
+	std::string new_s;
+	std::size_t found = str.find_first_of(":");
+	int i = 0;
 
-extern bool comInProgress;
-
-struct comNode{
-	char* cmd;
-	int numArgs;
-	char** args;
-	char* file_in; char* file_out;
-	char* stdin; char* stdout; char* stderr;
-	comNode* next;
-
-	comNode(){
-		cmd = NULL; numArgs = 0; args = new char*[numArgs+2]; args[0] = NULL; args[1] = NULL; file_in = NULL; file_out = NULL; stdin = NULL; stdout = NULL; stderr = NULL; next = NULL;
-	};
-	void changeCom(char* newcmd){
-		std::string c(newcmd);
-		std::string c_name = c.substr(c.find_last_of("/")+1);
-		cmd = new char[sizeof(newcmd)+1];
-		strcpy(cmd, newcmd);
-		if (args[0] != NULL)
-			free(args[0]);
-		args[0] = new char[c_name.length()+1];
-		strcpy(args[0], c_name.c_str());
-	};
-	void insertArg(char* arg){
-		numArgs++;
-		char** newArgs = new char*[numArgs+2];
-		for(int i=0; i<numArgs; i++){
-			newArgs[i] = new char[sizeof(args[i])+1];
-			strcpy(newArgs[i], args[i]);
+	//check if first character
+	//Find and separate all words
+	while(found != std::string::npos){
+		if(found == 0){
+			str = str.substr(found+1);
+			found = str.find_first_of(":");
 		}
-		newArgs[numArgs] = new char[sizeof(arg)+1];
-		strcpy(newArgs[numArgs], arg);
-		free (args);
-		args = newArgs;
-		args[numArgs+1] = NULL;
-	};
-	void changeInput(char* input){
-		stdin = new char[sizeof(input)+1];
-		strcpy(stdin, input);
-	};
-	void changeOutput(char* output){
-		stdout = new char[sizeof(output)+1];
-		strcpy(stdout, output);
-	};
-	void changeStdError(char* error){
-		stderr = new char[sizeof(error)+1];
-		strcpy(stderr, error);
-	};
-	void reset(){
-		free(cmd);
-		cmd = NULL;
-		numArgs = 2;
-		char** newArgs = new char*[numArgs+2];
-		for(int i=0;i<numArgs;i++){
-			newArgs[i] = NULL;
-		}
-		free(args);
-		args = newArgs;
-		free(file_in); free(file_out); free(stdin); free(stdout); free(stderr); free(next);
-		file_in = NULL; file_out = NULL; stdin = NULL; stdout = NULL; stderr = NULL; next = NULL;
+		//printf( "%d: %s\n", i, str.substr(0, found).c_str());
+		myPaths.push_back(str.substr(0,found).c_str());
+		i++;
+		str = str.substr(found+1);
+		found = str.find_first_of(":");
 	}
-};
+	//printf("%d: %s\n", i, str.c_str());
+	myPaths.push_back(str.c_str());
+}
+
+void clearPaths(){
+	int size = myPaths.size();
+	for (int i=0;i<size;i++)
+		myPaths.pop_back();
+}
